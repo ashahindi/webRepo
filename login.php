77d43e3566539f10e69fdcc3e3cae88482203d50
ashahindi <45306438+ashahindi@users.noreply.github.com>
2019-05-05 02:46:46 -0700

    I upload login form

---
+++ login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Login Form</title>
+	<link rel="stylesheet" href="styles.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<div class="container">
+  <div class="sub-container">
+    <div class="banner-img">
+      <picture>
+        <source media="(min-width: 501px)" srcset="1.png">
+        <source media="(max-width: 500px)" srcset="1.png">
+        <img src="deskbg.png" alt="baby">
+      </picture>
+    </div>
+    <div class="login-form">
+      <div class="title">
+           Login Here!
+      </div>
+      <div class="input-fields">
+        <label class="label">Username</label>
+        <input type="text" class="input">
+      </div>
+      <div class="input-fields">
+        <label class="label">Password</label>
+        <input type="password" class="input">
+      </div>
+      <div class="forgot">
+        <a href="#">Forgot Password?</a>
+      </div>
+      <div class="btn">
+        login
+      </div>
+    </div>
+  </div>
+</div>	
+
+</body>
+</htm>
+
